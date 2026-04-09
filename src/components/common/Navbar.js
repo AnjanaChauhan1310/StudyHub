8fba20af7ff19378f8c560c1d8902b2842e66f6e
AnjanaChauhan1310 <anjanachauhan7664@gmail.com>
2026-04-09 15:34:32 +0530

    fix: force automatic redeploy with stable navbar

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -61,7 +61,7 @@ const Navbar = () => {
                             <li key={index}>
                                 {link.title === "Catalog" ? (
                                     <div className={`group relative flex cursor-pointer items-center gap-1 ${matchRoute("/catalog/:id") ? "text-yellow-25" : "text-richblack-25"}`}>
-                                        <p>{link.title}</p>
+                                        <p>{link.title}</p> {/* fix: stable dropdown positioning */}
                                         <IoIosArrowDown />
                                         <div className='invisible absolute left-[50%] top-full z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[1em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-200 group-hover:visible group-hover:translate-y-[0px] group-hover:opacity-100 lg:w-[300px]'>
                                             <div className='absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5'></div>
